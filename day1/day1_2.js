8440e85c9a629d0d52122b5362fe7eb735d99a1b
Kathrin Holzmann <lelith@gmx.de>
2019-12-02 11:17:37 +0100

    refactor utils

--- day1/day1_2.js
+++ day1/day1_2.js
@@ -7,7 +7,9 @@ function calculateMass(mass) {
 
 try {
   // const data = utils.readInput('./example.txt');
-  const data = utils.readInput('./input.txt');
+  let data = utils.readInput('./input.txt');
+  data = utils.modDataNewline(data);
+
   const fuel = [];
   data.map((module) => {
     let fuelNeed = calculateMass(module);
