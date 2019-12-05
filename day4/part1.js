dce91cf6933290fc20685c9542cb6f1e6f6e8f00
Kathrin Holzmann <lelith@gmx.de>
2019-12-05 08:36:39 +0100

    WIP

--- day4/part1.js
+++ day4/part1.js
@@ -2,15 +2,27 @@ function splitToDigit(n) {
   return (`${n}`).split('').map((i) => Number(i));
 }
 try {
-  let password = 10;
-  const end = 30;
-  const possible = 0;
+  let password = 111;
+  const end = 150;
+  let possible = 0;
 
   while (password < end) {
     const digits = splitToDigit(password);
-    console.log(digits);
+    let hasDoubles = false;
+    let i = 0;
+    while (digits[i] >= digits[i + 1] && i <= digits.length) {
+      console.log(i);
+      if (digits[i] === digits[i + 1]) {
+        hasDoubles = true;
+      }
+      i += 1;
+    }
+    if (hasDoubles) {
+      possible += 1;
+    }
     password += 1;
   }
+  console.log(possible);
 } catch (e) {
   console.log('error');
 }
